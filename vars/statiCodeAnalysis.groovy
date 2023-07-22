6472b607c86307a0a8d504758844240ecd82c594
Noel Nondoh <noelnondoh@gmail.com>
2023-07-22 10:18:20 +0000

    Revert "update with maven version on staticCode"
    This reverts commit 5cd1310d7567b2f7d59e08c9b51eb1f94e2a857c.

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,13 +1,10 @@
 def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-        withMaven(maven:'Maven 3.9.3') {
-            if (isUnix()) {
-                sh 'mvn clean package sonar:sonar'
-            } else {
-                bat 'mvn clean package sonar:sonar'
-            }
-        }
-                  
+        if (isUnix()) {
+            sh 'mvn clean package sonar:sonar'
+        } else {
+            bat 'mvn clean package sonar:sonar'
+        }          
     }
 }
