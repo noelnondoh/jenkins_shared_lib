c69c8a1065d3e5df17608b19917aec328321169d
Noel Nondoh <noelnondoh@gmail.com>
2023-07-29 18:43:38 +0000

    update

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,10 @@
 def call(String project, String ImageTag, String hubUser){    
-    sh """
-        docker image build -t ${hubUser}/${project} . 
-        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-        docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-    """
+    // sh """
+    //     docker image build -t ${hubUser}/${project} . 
+    //     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+    //     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+    // """
+    sh 'sudo docker build -t ${hubUser}/${project} .'
 }
 
 // def call(String aws_account_id, String region, String ecr_repoName){
