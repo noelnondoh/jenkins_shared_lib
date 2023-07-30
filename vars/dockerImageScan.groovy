54bfe1fc46a78ba427d21b78b86216a56a2a8072
Noel Nondoh <noelnondoh@gmail.com>
2023-07-30 00:36:30 +0000

    update for dockerImageScan

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,10 +1,9 @@
-// def call(String project, String ImageTag, String hubUser){
-    
-//     sh """   
-//      trivy image ${hubUser}/${project}:latest > scan.txt
-//      cat scan.txt
-//     """
-// }
+def call(String ImageName, String ImageTag, String DockerHubUser){
+    sh """   
+     trivy image ${DockerHubUser}/${ImageName}:latest > scan.txt
+     cat scan.txt
+    """
+}
 
 // def call(String aws_account_id, String region, String ecr_repoName){
     
